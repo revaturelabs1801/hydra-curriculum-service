ca9ad2c7892b19219b8c8cde25d8d4e5c09961ff
Stephen Negron <negstephen@gmail.com>
2018-03-15 14:51:01 -0400

    added custom exceptions and internal curriculum controller

---
+++ hydra-curriculum-service/src/main/java/com/revature/hydra/curriculum/controller/InternalCurriculumController.java
@@ -0,0 +1,65 @@
+package com.revature.hydra.curriculum.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.hydra.curriculum.bean.Curriculum;
+import com.revature.hydra.curriculum.pojos.BamUser;
+import com.revature.hydra.curriculum.service.CurriculumService;
+
+@RestController
+@RequestMapping(value = "/")
+public class InternalCurriculumController {
+
+	@Autowired
+	CurriculumService curriculumService;
+
+	@PostMapping("deleteCurriculum")
+	public void deleteCurriculum(@RequestBody Curriculum curriculum){
+		curriculumService.deleteCurriculum(curriculum);
+	}
+	
+	@PostMapping("deleteCurriculumSubtopics")
+	public void deleteCurriculumSubtopics(@RequestBody Curriculum curriculum){
+		curriculumService.deleteCurriculumSubtopics(curriculum);
+	}
+	
+	@PostMapping("findAllCurriculumByName/{name}")
+	public List<Curriculum> findAllCurriculumByName(@PathVariable String name){
+		return curriculumService.findAllCurriculumByName(name);
+	}
+	
+	@PostMapping("findAllCurriculumByNameAndIsMaster/{name}")
+	public List<Curriculum> findAllCurriculumByNameAndIsMaster(@PathVariable String name, @RequestBody Integer isMaster){
+		return curriculumService.findAllCurriculumByNameAndIsMaster(name, isMaster);
+	}
+	
+	@SuppressWarnings("rawtypes")
+	@PostMapping("getAllCurriculum")
+	public Map<String, List> getAllCurriculum(@RequestBody List<BamUser> users){
+		return curriculumService.getAllCurriculum(users);
+	}
+	
+	@PostMapping("getCuricullumById/{id}")
+	public Curriculum getCuricullumById(@PathVariable Integer id){
+		return curriculumService.getCuricullumById(id);
+	}
+	
+	@PostMapping("getCuricullumByIdKeepPwd/{id}")
+	public Curriculum getCuricullumByIdKeepPwd(@PathVariable Integer id){
+		return curriculumService.getCuricullumByIdKeepPwd(id);
+	}
+	
+	@PostMapping("save")
+	public Curriculum save(@RequestBody Curriculum curriculum){
+		return curriculumService.save(curriculum);
+	}
+
+}
